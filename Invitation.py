990e73dfa108328bdb150ffd09ac5a730af925cd
Sandhya Devireddy <sandya.devireddy2007@gmail.com>
2023-02-14 15:41:26 +0530

    Invitation program

---
+++ Invitation.py
@@ -0,0 +1,6 @@
+Player = ['kohli','Dhoni','Raghul']
+message=" You are invited for dinner today"
+print(Player[0].title()+message)
+print(Player[1].title()+message)
+print(Player[2].title()+message)
+
